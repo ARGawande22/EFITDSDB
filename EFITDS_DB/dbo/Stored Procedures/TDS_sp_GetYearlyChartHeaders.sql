f76b6def2f7040fee3bf53aa62f0443cb46f1089
Akash R Gawande <akashrgawande@gmail.com>
2026-06-20 16:56:27 +0530

    EFI-004 New SP created for loading Yearly Chart headers

---
+++ EFITDS_DB/dbo/Stored Procedures/TDS_sp_GetYearlyChartHeaders.sql
@@ -0,0 +1,43 @@
+﻿
+Create   Procedure [dbo].[TDS_sp_GetYearlyChartHeaders]
+	@Regards Nvarchar(20)=null
+
+--***********************************************************
+--***
+--*** Created On 20 Jun 2026 By A.R.Gawande
+--***
+--***********************************************************
+AS
+BEGIN
+
+	SET NOCOUNT ON 
+	SET ANSI_NULLS ON
+	SET ANSI_WARNINGS ON
+
+	DECLARE @ErrMsg Nvarchar(255),
+			@Status Char(1)
+
+	--Get Yearly Chart Headers Information
+	Select
+			ych.Header_Id,
+			ych.OrderSeq,
+			ych.Regards,
+			ych.Header_Name,
+			ych.Display_Name,
+			ych.IsYearlyHeader,
+			ych.[Status]
+		FROM [dbo].[TDS_t_YearlyChart_Headers] ych with(nolock)
+		WHERE (@Regards IS NULL OR ych.Regards=@Regards)
+		ORDER BY ych.OrderSeq
+
+	RETURN(0)
+
+spError:
+	IF(ISNULL(DATALENGTH(@ErrMsg),0))>0
+	BEGIN
+		SELECT @ErrMsg=@ErrMsg
+		RAISERROR(@ErrMsg,18,1)
+	END
+
+	RETURN(-1)
+END
---
+++ EFITDS_DB/dbo/Stored Procedures/TDS_sp_GetYearlyChartRefHeaders.sql
@@ -0,0 +1,50 @@
+﻿
+Create   Procedure [dbo].[TDS_sp_GetYearlyChartRefHeaders]
+	@Header_Id int=0
+
+--***********************************************************
+--***
+--*** Created On 18 Apr 2025 By A.R.Gawande
+--***
+--***********************************************************
+AS
+BEGIN
+
+	SET NOCOUNT ON 
+	SET ANSI_NULLS ON
+	SET ANSI_WARNINGS ON
+
+	DECLARE @ErrMsg Nvarchar(255),
+			@Status Char(1)
+
+	IF @Header_Id is null
+	BEGIN
+	   Select @ErrMsg='TDS_sp_GetYearlyChartRefHeaders: Header id parameter is required.'
+	   GOTO spError
+	END
+
+	--Get Sevaarth Sub Header Information
+	    Select
+			ych.Header_Id,			
+			ycrh.Ref_Id,			
+			ycrh.Ref_HeaderId,
+			ycrh.Ref_EstimateHeaderId,
+			ycrh.Ref_MainHeader_Name,
+			ycrh.IsIncludeCalSheet,
+			ycrh.Status
+		FROM [dbo].[TDS_t_YearlyChart_Headers] ych with(nolock)
+			RIGHT JOIN [dbo].[TDS_t_YearlyChart_RefHeaders] ycrh with(nolock) ON ych.Header_Id=ycrh.Header_Id
+		WHERE (@Header_Id=0 OR ych.Header_Id=@Header_Id)
+		ORDER BY ycrh.Ref_Id
+
+	RETURN(0)
+
+spError:
+	IF(ISNULL(DATALENGTH(@ErrMsg),0))>0
+	BEGIN
+		SELECT @ErrMsg=@ErrMsg
+		RAISERROR(@ErrMsg,18,1)
+	END
+
+	RETURN(-1)
+END
